2c7154d6254eb42f8c516081f64d73e102761fd5
saevar bjornsson <saevarl3@gmail.com>
2022-11-28 11:09:27 +0000

    bla

--- src/screens/homescreen/index.js
+++ src/screens/homescreen/index.js
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect }from 'react'
+import { useNavigation } from '@react-navigation/native'; 
 
 const HomeScreen = () => {
+    const navigation = useNavigation(); 
+    
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+
+    
+
+
   return (
+    <SafeAreaView>
     <View>
       <Text className="text-red-500">HomeScreen</Text>
     </View>
+    </SafeAreaView>
   )
 }
 
